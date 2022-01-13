9e9f49846b3ea761639c5986e4b9d8ea50498987
Eric Moran <emoran0403@gmail.com>
2022-01-13 15:06:26 -0500

    almost got party mode

--- script.js
+++ script.js
@@ -21,6 +21,19 @@ myButton.onclick = divAdder //sets the onclick action to run the divAdder functi
 */
 let n = 1 // this variable serves as the id counter so each new div will have an incrementally new value
 
+/*
+9: When a square is clicked, it should change to a random background color (You can randomly choose from a set of colors you predefine if you wish)
+*/
+
+function randomColor() { //this function will return a random rgb color r, g, b
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+
+}
+
 function divAdder() { //creates the list adder function
     var sqDiv = document.createElement('div');  // creates a div item
     sqDiv.innerHTML = `I am div ${n}`;  // sets the text on the div to be "I am div n, where n starts at 1 and increases on each button press" >>yay template literals!
@@ -33,14 +46,33 @@ function divAdder() { //creates the list adder function
 
 
     n++; // increments n by 1 so that new ids will increase
-
-    /*
-    6: When there is no more room, they should wrap to the next line
-    */
     sqDiv.className = "sqDiv"; //gives the div created above a class name
-
     bigDiv.appendChild(sqDiv);  // appends the square divs to the big div in the document body
 
-    
-   //sqDiv.addEventListener("mouseover", )
+    sqDiv.debounceId = 0;
+
+sqDiv.addEventListener("click", function (event) {
+            // If a setTimeout ID exists for the element
+            if (sqDiv.debounceId) {
+                // then cancel it and reset it to zero
+                window.clearTimeout(sqDiv.debounceId);
+                sqDiv.debounceId = 0;
+            }
+
+            
+            // If a single click fires (aka event.detail is 1)
+            if (event.detail === 1) {
+                // then create a new setTimeout with single click functionality
+                // and save the new setTimeout ID to the element.debounceID property
+                sqDiv.debounceId = setTimeout(function () {
+                    console.log("Single!");
+                    sqDiv.style.backgroundColor = randomColor();
+                }, 500);
+                // Else if it's a double click, do double click actions
+            } else if (event.detail === 2) {
+                console.log("Double");
+            }
+            });
 }
+
+
