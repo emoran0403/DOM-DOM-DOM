e9f8922b06f5fd4591c8ddc26b7f746ec3dbe321
Eric Moran <emoran0403@gmail.com>
2022-01-13 14:48:28 -0500

    title hover text added

--- script.js
+++ script.js
@@ -16,15 +16,31 @@ document.body.appendChild(bigDiv); //appends the div to the body of the document
 */
 myButton.onclick = divAdder //sets the onclick action to run the divAdder function
 
+/*
+7: Each div should have an id with a numerical value equivalent to the total number of squares that have been added so far since page load when the Add Square button is clicked
+*/
 let n = 1 // this variable serves as the id counter so each new div will have an incrementally new value
 
 function divAdder() { //creates the list adder function
     var sqDiv = document.createElement('div');  // creates a div item
     sqDiv.innerHTML = `I am div ${n}`;  // sets the text on the div to be "I am div n, where n starts at 1 and increases on each button press" >>yay template literals!
     sqDiv.setAttribute("id", n); // sets the attribute to the current n
+
+    /*
+    8: When hovering over a square, the value of the square's id should display centered in the square, and disappear when the cursor is no longer over the square
+    */
+    sqDiv.setAttribute("title", n); //sets the element title to be the element's id. the title will show up as a tooltip when the mouse hovers over the element
+
+
     n++; // increments n by 1 so that new ids will increase
 
+    /*
+    6: When there is no more room, they should wrap to the next line
+    */
     sqDiv.className = "sqDiv"; //gives the div created above a class name
 
     bigDiv.appendChild(sqDiv);  // appends the square divs to the big div in the document body
+
+    
+   //sqDiv.addEventListener("mouseover", )
 }
